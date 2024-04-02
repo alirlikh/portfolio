f147b5548d13f973c2709423e7aa99c9c6171260
Alireza Jalili <alirlikh@gmail.com>
2024-04-02 15:09:20 +0330

    feat:add font and color to config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     // extend: {
@@ -14,7 +18,26 @@ const config: Config = {
     //       "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
     //   },
     // },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px"
+    },
+    colors: {
+      "gray-900": `rgb(var(--color-gray-900)`,
+      "gray-800": `rgb(var(--color-gray-800)`,
+      "gray-700": `rgb(var(--color-gray-700)`,
+      "gray-400": `rgb(var(--color-gray-400)`,
+      "gray-300": `rgb(var(--color-gray-300)`,
+      "green-300": `rgb(var(--color-green-500)`,
+      "purple-300": `rgb(var(--color-purple-500)`,
+      " white": `rgb(var(--color-white)`
+    },
+    fontFamily: {
+      body: ["raleway-medium", "Open Sans"],
+      heading: ["raleway-bold"]
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
