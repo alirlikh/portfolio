eb0a0452587ddcf31e2481645636dcb992c20c21
Alireza Jalili <alirlikh@gmail.com>
2024-04-03 15:57:24 +0330

    fix: tailwind color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,29 +12,29 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      },
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.19, 1, 0.22, 1)"
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px"
+      },
+      colors: {
+        "gray-900": `rgb(var(--color-gray-900))`,
+        "gray-800": `rgb(var(--color-gray-800))`,
+        "gray-700": `rgb(var(--color-gray-700))`,
+        "gray-400": `rgb(var(--color-gray-400))`,
+        "gray-300": `rgb(var(--color-gray-300))`,
+        "green-300": `rgb(var(--color-green-500))`,
+        "purple-300": `rgb(var(--color-purple-500))`,
+        white: `rgb(var(--color-white))`
+      },
+      fontFamily: {
+        body: ["raleway-medium", "Open Sans"],
+        heading: ["raleway-bold"]
       }
-    },
-    transitionTimingFunction: {
-      "in-expo": "cubic-bezier(0.19, 1, 0.22, 1)"
-    },
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px"
-    },
-    colors: {
-      "gray-900": `rgb(var(--color-gray-900)`,
-      "gray-800": `rgb(var(--color-gray-800)`,
-      "gray-700": `rgb(var(--color-gray-700)`,
-      "gray-400": `rgb(var(--color-gray-400)`,
-      "gray-300": `rgb(var(--color-gray-300)`,
-      "green-300": `rgb(var(--color-green-500)`,
-      "purple-300": `rgb(var(--color-purple-500)`,
-      " white": `rgb(var(--color-white)`
-    },
-    fontFamily: {
-      body: ["raleway-medium", "Open Sans"],
-      heading: ["raleway-bold"]
     }
   },
   plugins: []
