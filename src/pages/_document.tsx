f74010aaa2c868f950d1fbbdc06a6c359f53f245
Alireza Jalili <alirlikh@gmail.com>
2024-04-03 17:50:28 +0330

    fix:ltr

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,18 @@
-import Document, {
-     Html,
-     Head,
-     Main,
-     NextScript,
-     DocumentContext,
-   } from "next/document";
-   
-   export default class CustomDocument extends Document {
-     static async getInitialProps(ctx: DocumentContext) {
-       return Document.getInitialProps(ctx);
-     }
-     render() {
-       return (
-         <Html>
-           <Head />
-           <body dir="rtl">
-             <Main />
-             <NextScript />
-           </body>
-         </Html>
-       );
-     }
-   }
-   
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document"
+
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return Document.getInitialProps(ctx)
+  }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body dir="ltr">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
