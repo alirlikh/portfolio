a267a7e449baf70d83e4b0eb3c240ea6b50b6467
Alireza Jalili <alirlikh@gmail.com>
2024-04-04 16:27:19 +0330

    fix: icon

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,16 @@
+import { Github } from "../icons/github"
+import { Linkdin } from "../icons/linkdin"
+import { Mail } from "../icons/mail"
+
 export default function Footer() {
   return (
     <div className="flex flex-col items-start m-4 ">
       <div className="w-full py-2 bg-gray-850 px-4 rounded-full flex flex-row justify-between items-center">
         <p className="text-gray-400 text-sm m-2">Follow me</p>
-        <div className="flex flex-row m-2">
-          <div className="bg-green-700 w-2 m-2">1</div>
-          <div className="bg-yellow-300 w-2 m-2">2</div>
-          <div className="bg-yellow-300 w-2 m-2">3</div>
+        <div className="flex flex-row items-center w-24 justify-around">
+          <Github />
+          <Linkdin />
+          <Mail />
         </div>
       </div>
       <div className="m-3">
