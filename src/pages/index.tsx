e934ecf411df0b613983ace138cdf688f8c34576
Alireza Jalili <alirlikh@gmail.com>
2024-04-05 18:33:15 +0330

    fix: width in main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           </span>
         </div>
       </div>
-      <div className="my-6 mx-auto p-2 w-1/3 md:max-w-md ">
+      <div className="my-6 mx-auto p-2 w-1/3 md:max-w-md md:m-0 ">
         <Image
           src={profile}
           alt="profile image"
