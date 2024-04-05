f3bc5b1a47d9e55fbfece0a42ceb28aa5dae098c
Alireza Jalili <alirlikh@gmail.com>
2024-04-05 15:29:54 +0330

    feat: styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,30 +6,30 @@ import { Figma } from "../icons/figma"
 
 export default function Header() {
   return (
-    <header className="p-3 fixed  left-1/2 transform -translate-x-1/2 ">
-      <nav className="bg-gray-700 backdrop-blur-lg rounded-full px-5 py-1 ">
-        <ul className="flex flex-row p-2 justify-between ">
-          <li className="m-3 p-1">
+    <header className="fixed p-3 left-1/2 transform -translate-x-1/2  max-w-80 bottom-2 md:max-w-[620px] md:top-0">
+      <nav className="bg-gray-300 backdrop-blur-xl rounded-full px-5 py-1 h-[70px] overflow-auto no-scrollbar">
+        <ul className="flex flex-row p-2 justify-between *:mx-3 *:p-1 *:my-1 *:transition-all  *:duration-150 *:delay-50">
+          <li className="hover:brightness-[0.6]">
             <NavLink href={"/"}>Alireza</NavLink>
           </li>
-          <li className="m-3 p-1">
-            <NavLink href={"/education"}>education</NavLink>
-          </li>
-          <li className="m-3 p-1">
+          <li className="hover:brightness-[0.6]">
             <NavLink href={"/experience"}>experience</NavLink>
           </li>
-          <li className="m-3 w-32 p-1">
-            <div className="flex flex-row items-center justify-between">
-              <NavLink href={"#"}>
+          <li className="hover:brightness-[0.6]">
+            <NavLink href={"/education"}>education</NavLink>
+          </li>
+          <li className="w-32">
+            <div className="hidden md:flex flex-row  items-center justify-between  *:transition-all  *:duration-150 *:delay-50  ">
+              <NavLink href={"#"} className="hover:brightness-[0.6]">
                 <Figma></Figma>
               </NavLink>
-              <NavLink href={"#"}>
+              <NavLink href={"#"} className="hover:brightness-[0.6]">
                 <Github></Github>
               </NavLink>
-              <NavLink href={"#"}>
+              <NavLink href={"#"} className="hover:brightness-[0.6]">
                 <Linkdin></Linkdin>
               </NavLink>
-              <NavLink href={"#"}>
+              <NavLink href={"#"} className="hover:brightness-[0.6]">
                 <Mail></Mail>
               </NavLink>
             </div>
