675237ad3db9ff8d5b143f08f19746817e7ee86d
Alireza Jalili <alirlikh@gmail.com>
2024-04-06 12:25:13 +0330

    fix: link hover style

--- src/components/link/NavLink.tsx
+++ src/components/link/NavLink.tsx
@@ -11,7 +11,13 @@ const NavLink: React.FC<LinkProps & HTMLAttributes<HTMLAnchorElement>> = ({
   const router = useRouter()
   let activeNav = href === `${router.pathname}`
   return (
-    <Link href={href} className={`${activeNav ? "text-purple-300" : ""} ${className}`} {...props}>
+    <Link
+      href={href}
+      className={`${
+        activeNav ? "text-purple-300" : ""
+      } ${className} transition-all  duration-150 delay-50 hover:brightness-[0.6]`}
+      {...props}
+    >
       {children}
     </Link>
   )
