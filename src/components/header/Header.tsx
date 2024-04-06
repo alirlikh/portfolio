6c4b239c24d473057849b89c3c3618661fb80a2b
Alireza Jalili <alirlikh@gmail.com>
2024-04-06 12:24:28 +0330

    fix: link href

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,29 +8,52 @@ export default function Header() {
   return (
     <header className="fixed p-3 left-1/2 transform -translate-x-1/2  max-w-80 bottom-2 md:max-w-[620px] md:top-0">
       <nav className="bg-gray-300 backdrop-blur-xl rounded-full px-5 py-1 h-[70px] overflow-auto no-scrollbar">
-        <ul className="flex flex-row p-2 justify-between *:mx-3 *:p-1 *:my-1 *:transition-all  *:duration-150 *:delay-50">
-          <li className="hover:brightness-[0.6]">
+        <ul className="flex flex-row p-2 justify-between *:mx-3 *:p-1 *:my-1 ">
+          <li>
             <NavLink href={"/"}>Alireza</NavLink>
           </li>
-          <li className="hover:brightness-[0.6]">
+          <li>
             <NavLink href={"/experience"}>experience</NavLink>
           </li>
-          <li className="hover:brightness-[0.6]">
+          <li>
             <NavLink href={"/education"}>education</NavLink>
           </li>
           <li className="w-32">
-            <div className="hidden md:flex flex-row  items-center justify-between  *:transition-all  *:duration-150 *:delay-50  ">
-              <NavLink href={"#"} className="hover:brightness-[0.6]">
-                <Figma></Figma>
+            <div className="hidden md:flex flex-row  items-center justify-between ">
+              <NavLink href={"https://www.google.com"} passHref legacyBehavior>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-all  duration-150 delay-50 hover:brightness-[0.6]"
+                >
+                  <Figma></Figma>
+                </a>
               </NavLink>
-              <NavLink href={"#"} className="hover:brightness-[0.6]">
-                <Github></Github>
+              <NavLink href={"https://www.google.com"} passHref legacyBehavior>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-all  duration-150 delay-50 hover:brightness-[0.6]"
+                >
+                  <Linkdin></Linkdin>
+                </a>
               </NavLink>
-              <NavLink href={"#"} className="hover:brightness-[0.6]">
-                <Linkdin></Linkdin>
+              <NavLink href={"https://www.google.com"} passHref legacyBehavior>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transition-all  duration-150 delay-50 hover:brightness-[0.6]"
+                >
+                  <Github></Github>
+                </a>
               </NavLink>
-              <NavLink href={"#"} className="hover:brightness-[0.6]">
-                <Mail></Mail>
+              <NavLink href={"mailto:john@example.com"} passHref legacyBehavior>
+                <a
+                  href="mailto:john@example.com"
+                  className="transition-all  duration-150 delay-50 hover:brightness-[0.6]"
+                >
+                  <Mail></Mail>
+                </a>
               </NavLink>
             </div>
           </li>
