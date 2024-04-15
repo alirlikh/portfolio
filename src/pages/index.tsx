180ff2b3cc26dfab1b38e93b528a275ce6294ce1
Alireza Jalili <alirlikh@gmail.com>
2024-04-15 17:17:52 +0330

    fix: text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       </section>
       <section className="p-2 my-48">
         <div className="flex flex-col justify-center items-center *:p-3 *:my-7 md:*:my-4 *:font-extrabold *:text-[80px] *:leading-[130px]">
-          <h2 className="text-violet-300">Codding</h2>
+          <h2 className="text-violet-300">Coding</h2>
           <h2 className="text-violet-500">Javascript</h2>
           <h2 className="text-violet-700">React</h2>
           <h2 className="text-violet-900">Coffe</h2>
