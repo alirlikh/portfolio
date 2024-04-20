eb8f50e15247621fb10f4f5192df7ad9cd8de92b
Alireza Jalili <alirlikh@gmail.com>
2024-04-20 11:40:27 +0330

    feat: add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+    scope: "/",
+    sw: "service-worker.js",
+    skipWaiting: true,
+    version: "v1.0.0"
+  }
+}
 
-export default nextConfig;
+export default nextConfig
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class CustomDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
