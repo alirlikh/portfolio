3702c5ee17e7a87df9de94cfa623ebc5829ffb5e
Alireza Jalili <alirlikh@gmail.com>
2024-08-06 19:02:04 +0330

    feat: add shadow to main page

--- src/components/sliderSection/SliderSection.tsx
+++ src/components/sliderSection/SliderSection.tsx
@@ -4,8 +4,8 @@ import Slider from "@/components/slider/Slider"
 function SliderSection() {
   const date = new Date()
   return (
-    <section className=" flex flex-col items-center my-28 md:flex-row md:justify-between mx-auto md:mt-56 ">
-      <div className="flex items-center justify-center p-2 my-6 w-[10%] md:basis-[20%]">
+    <section className=" flex flex-col items-center my-28 md:flex-row md:justify-between mx-auto md:mt-56  ">
+      <div className="flex items-center justify-center p-2 my-6 lg:w-[10%] md:basis-[20%] bg-[url('/images/Shadow.svg')] bg-no-repeat bg-center bg-cover h-screen w-full">
         <p className="flex flex-col md:text-start text-gray-400 text-[16px] font-normal text-center">
           + {date.getFullYear() - 2022} years
           <span className="text-white font-bold text-[80px] text-center p-1 leading-[100px]">
