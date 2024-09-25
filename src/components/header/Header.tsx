d1b9fd036e506f3fd469076bc589d85c2f5ff6b0
Alireza Jalili <alirlikh@gmail.com>
2024-09-25 16:53:28 +0330

    fix: menu  map item

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,38 +4,57 @@ import { Linkdin } from "../icons/linkdin"
 import { Mail } from "../icons/mail"
 
 export default function Header() {
+  interface Menu {
+    id: number
+    title: string
+    href: string
+    isIcon: boolean
+    icon?: JSX.Element
+  }
+
+  const menuItem: Menu[] = [
+    { id: 1, title: "Alireza", href: "/", isIcon: false },
+    { id: 2, title: "project", href: "/project", isIcon: false },
+    { id: 3, title: "experience", href: "/experience", isIcon: false },
+    { id: 4, title: "education", href: "/education", isIcon: false },
+    {
+      id: 5,
+      title: "Linkdin",
+      href: "https://linkedin.com/in/alireza-jalili",
+      isIcon: true,
+      icon: <Linkdin />
+    },
+    { id: 5, title: "Github", href: "https://github.com/alirlikh", isIcon: true, icon: <Github /> },
+    {
+      id: 5,
+      title: "Mail",
+      href: "mailto:alirezajalili.pm@gmail.com",
+      isIcon: true,
+      icon: <Mail />
+    }
+  ]
   return (
     <header className="fixed p-3 left-1/2 transform -translate-x-1/2  max-w-80 bottom-2 md:max-w-[622px] md:top-0 md:bottom-auto z-10 ">
       <nav className="bg-header backdrop-blur-xl rounded-full px-5 py-2 h-[83px] overflow-auto no-scrollbar">
-        <ul className="flex flex-row p-2 justify-between *:mx-3 *:p-1 *:my-1 items-center ">
-          <li>
-            <NavLink href={"/"}>Alireza</NavLink>
-          </li>
-          <li>
-            <NavLink href={"/experience"}>experience</NavLink>
-          </li>
-          <li>
-            <NavLink href={"/education"}>education</NavLink>
-          </li>
-          <li className="w-32">
-            <div className="hidden md:flex flex-row  items-center justify-between ">
-              <NavLink href={"https://linkedin.com/in/alireza-jalili"} passHref legacyBehavior>
-                <a target="_blank" rel="noopener noreferrer" aria-label="github">
-                  <Linkdin></Linkdin>
-                </a>
-              </NavLink>
-              <NavLink href={"https://github.com/alirlikh"} passHref legacyBehavior>
-                <a target="_blank" rel="noopener noreferrer" aria-label="linkdin">
-                  <Github></Github>
-                </a>
-              </NavLink>
-              <NavLink href={"mailto:alirezajalili.pm@gmail.com"} passHref legacyBehavior>
-                <a href="mailto:alirezajalili.pm@gmail.com" aria-label="mail">
-                  <Mail></Mail>
-                </a>
-              </NavLink>
-            </div>
-          </li>
+        <ul className="flex flex-row p-2 justify-between  items-center ">
+          {menuItem?.map((item: Menu) =>
+            !item.isIcon ? (
+              <li className="mx-3 p-1 my-1 flex-grow flex-1 flex-shrink " key={item.id}>
+                <NavLink href={item.href}>{item.title}</NavLink>
+              </li>
+            ) : (
+              <li
+                key={item.id}
+                className="hidden md:flex flex-row  items-center justify-between m-0 p-1"
+              >
+                <NavLink href={item.href} passHref legacyBehavior>
+                  <a target="_blank" rel="noopener noreferrer" aria-label={item.title}>
+                    {item.icon}
+                  </a>
+                </NavLink>
+              </li>
+            )
+          )}
         </ul>
       </nav>
     </header>
