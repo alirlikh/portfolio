8b90b9d81050f12b1a8df387d1013b407e3b2669
Alireza Jalili <alirlikh@gmail.com>
2024-09-25 17:22:37 +0330

    feat: add project routes

---
+++ src/pages/project/index.tsx
@@ -0,0 +1,9 @@
+const Project = () => {
+  return (
+    <div className="h-svh flex justify-center items-center">
+      <p>In Designing Faze, Comming Soon...</p>
+    </div>
+  )
+}
+
+export default Project
