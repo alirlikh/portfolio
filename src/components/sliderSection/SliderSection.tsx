dd14831e1ee5c3aaebc202424dc577705cf99211
Alireza Jalili <alirlikh@gmail.com>
2025-01-05 19:36:07 +0330

    fix: date

--- src/components/sliderSection/SliderSection.tsx
+++ src/components/sliderSection/SliderSection.tsx
@@ -7,7 +7,7 @@ function SliderSection() {
     <section className=" flex flex-col items-center my-28 md:flex-row md:justify-between mx-auto md:mt-56  ">
       <div className="flex items-center justify-center p-2 my-6 lg:w-[10%] md:basis-[20%] bg-[url('/images/Shadow.svg')] bg-no-repeat bg-center bg-cover h-screen w-full">
         <p className="flex flex-col md:text-start text-gray-400 text-[16px] font-normal text-center">
-          + {date.getFullYear() - 2022} years
+          + {date.getFullYear() - 2023} years
           <span className="text-white font-bold text-[80px] text-center p-1 leading-[100px]">
             XP
           </span>
