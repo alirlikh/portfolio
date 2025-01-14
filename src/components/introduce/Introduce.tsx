e4fba058d53b310eed9e7969cbee1e07fe48a0f9
Alireza Jalili <alirlikh@gmail.com>
2025-01-14 20:49:49 +0330

    fix: avatar animation

--- src/components/introduce/Introduce.tsx
+++ src/components/introduce/Introduce.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion"
 import Image from "next/image"
-import avatar from "../../../public/images/avatar.svg"
+import avatar from "../../../public/images/avatar.jpg"
 import Link from "next/link"
 import React from "react"
 import { cvUrl } from "@/Data/data"
@@ -8,7 +8,7 @@ import DownloadButton from "@/components/downloadButton/DownloadButton"
 
 function Introduce() {
   return (
-    <section className=" py-2 px-6 md:px-28 flex flex-col-reverse items-center lg:flex-row md:justify-between md:mt-28">
+    <section className="container mx-auto py-2 px-6 md:px-28 flex flex-col-reverse items-center lg:flex-row md:justify-between md:mt-28">
       <motion.div
         className="flex flex-col w-full  md:max-w-4xl items-center lg:items-baseline"
         initial={{ x: -100, opacity: 0 }}
@@ -39,7 +39,7 @@ function Introduce() {
         </div>
       </motion.div>
       <motion.div
-        className="my-6 mx-auto p-2  md:m-0 relative"
+        className="my-6 mx-auto p-2  md:m-0 relative "
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.2, duration: 0.5 }}
@@ -48,7 +48,7 @@ function Introduce() {
           src={avatar}
           priority
           alt="profile image"
-          className="mx-auto aspect-auto max-w-36 md:max-w-56 lg:max-w-80 scale-150 "
+          className="mx-auto aspect-auto max-w-36 md:max-w-56 lg:max-w-80 scale-125 picture-raduis "
         />
         <motion.div
           className="text-3xl cursor-pointer -left-[11px] -bottom-[4px] z-10 absolute w-[160px] md:w-[280px] md:text-6xl md:left-[-30px] md:bottom-[-12px]  "
