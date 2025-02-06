9b0573c6a37ae5b92bd9f499be90e617d18724ad
Alireza Jalili <alirlikh@gmail.com>
2025-02-06 17:05:46 +0330

    fix: project card style

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -18,23 +18,29 @@ export default function ProjectCard({ project }: { project: Project }) {
       <motion.div
         className="max-w-96  rounded-[40px] bg-gray-800 w-full mx-8 mt-20 border-2 border-gray-700"
         initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
+        animate={{
+          y: 0,
+          opacity: 1,
+          transition: {
+            delay: 0.2
+            // duration: 0.7
+          }
+        }}
         whileHover={{ y: -5, opacity: 1 }}
       >
         {/* image */}
         <div className="w-5/6 mx-auto place-items-center -mt-12 relative h-52  ">
           <Image
             priority
-            // placeholder="blur"
             src={image}
             alt={`${name} project image`}
             fill
-            className="object-cover rounded-[40px] drop-shadow-[0px_5px_3px_rgba(var(--color-purple-300),0.5)] "
+            className="object- rounded-[40px] border-2   border-dashed p-2 border-purple-300 "
           />
         </div>
         {/* content */}
         <div className="mx-auto p-3 mt-5 text-center space-y-6 mb-3">
-          <h4 className=" font-bold text-3xl capitalize py-3 underline underline-offset-[16px] decoration-purple-300 decoration-dashed">
+          <h4 className=" font-bold text-3xl capitalize py-3 underline underline-offset-[16px] decoration-purple-300 ">
             {name}
           </h4>
           {/* button */}
