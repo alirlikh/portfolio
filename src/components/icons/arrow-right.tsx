4671ceba0dde71a1b0ffd0ceb3787c50bf0948e3
Alireza Jalili <alirlikh@gmail.com>
2025-02-06 15:37:55 +0330

    fix: Invalid DOM property `stroke-linecap`

--- src/components/icons/arrow-right.tsx
+++ src/components/icons/arrow-right.tsx
@@ -8,14 +8,14 @@ export const ArrowRight = ({ ...props }) => {
       height="48"
       {...props}
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g id="SVGRepo_tracerCarrier" stroke-linecap="round" strokeLinejoin="round"></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
         <path
           d="M4 12H20M20 12L16 8M20 12L16 16"
           stroke="#000000"
-          stroke-width="2"
+          strokeWidth="2"
           stroke-linecap="round"
           strokeLinejoin="round"
         ></path>{" "}
--- src/components/icons/arrow-up.tsx
+++ src/components/icons/arrow-up.tsx
@@ -9,13 +9,13 @@ export const ArrowUp = ({ ...props }) => {
       height="64px"
       viewBox="-30.02 -30.02 90.06 90.06"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke="#CCCCCC"
-        stroke-width="1.140798"
+        strokeWidth="1.140798"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
