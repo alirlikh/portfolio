60a87a6ba503dee76d67a63be2e691c8c579bde5
Alireza Jalili <alirlikh@gmail.com>
2025-02-09 09:29:10 +0330

    feat: add stagger animation in project list

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { motion } from "framer-motion"
+import { animate, motion } from "framer-motion"
 import { Github } from "../icons/github"
 import { Earth } from "../icons/earth"
 
@@ -13,20 +13,26 @@ export type Project = {
 
 export default function ProjectCard({ project }: { project: Project }) {
   const { image, githubUrl, demoUrl, name } = project
+
+  const variants = {
+    initial: { y: -100, opacity: 0 },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.7,
+        staggerChildren: 0.5,
+        delayChildren: 0.1
+      }
+    }
+  }
+
   return (
     <>
       <motion.div
         className="max-w-96  rounded-[40px] bg-gray-800 w-full mx-8 mt-20 border-2 border-gray-700"
-        initial={{ y: -100, opacity: 0 }}
-        animate={{
-          y: 0,
-          opacity: 1,
-          transition: {
-            delay: 0.2
-            // duration: 0.7
-          }
-        }}
-        whileHover={{ y: -5, opacity: 1 }}
+        whileHover={{ y: -10 }}
+        variants={variants}
       >
         {/* image */}
         <div className="w-5/6 mx-auto place-items-center -mt-12 relative h-52  ">
@@ -35,7 +41,7 @@ export default function ProjectCard({ project }: { project: Project }) {
             src={image}
             alt={`${name} project image`}
             fill
-            className="object- rounded-[40px] border-2   border-dashed p-2 border-purple-300 "
+            className="object-cover rounded-[40px] border-2   border-dashed p-2 border-purple-300 "
           />
         </div>
         {/* content */}
--- src/components/projectList/ProjectList.tsx
+++ src/components/projectList/ProjectList.tsx
@@ -1,12 +1,33 @@
 import ProjectCard from "../projectCard/ProjectCard"
 import { projects } from "@/Data/data"
+import { motion } from "framer-motion"
 
 export default function ProjectList() {
+  const animationVariants = {
+    initial: {
+      y: -100,
+      opacity: 0
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.7,
+        staggerChildren: 0.5,
+        delayChildren: 0.3
+      }
+    }
+  }
   return (
-    <div className="py-40 mx-auto flex flex-row items-center justify-center flex-wrap">
+    <motion.div
+      variants={animationVariants}
+      initial={"initial"}
+      animate={"animate"}
+      className="py-40 mx-auto flex flex-row items-center justify-center flex-wrap"
+    >
       {projects.map((project, index) => {
         return <ProjectCard key={index} project={project} />
       })}
-    </div>
+    </motion.div>
   )
 }
